50cc3ccd5c8361bc8a83d89d3ccf45a4526865d5
lukyer <lukyer@gmail.com>
2022-10-07 22:17:19 +0200

    first version

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,15 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import {
+  Service,
+  PlatformAccessory,
+  CharacteristicValue,
+  CharacteristicSetCallback,
+  CharacteristicGetCallback,
+  Nullable,
+} from 'homebridge';
 
 import { ExampleHomebridgePlatform } from './platform';
+
+import got from 'got';
 
 /**
  * Platform Accessory
@@ -14,15 +23,39 @@ export class ExamplePlatformAccessory {
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+  // private exampleStates = {
+  //   On: false,
+  //   Brightness: 100,
+  // };
+
+  private prevState: Nullable<CharacteristicValue> = null;
+  private refreshIntervalMs = 1000;
+  private statusUrl: string;
+  private toggleUrl: string;
+  private bearerToken: string;
+  private authHeaders = {};
+
+  private fixer = 0;
 
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
+
+    this.refreshIntervalMs = this.platform.config.refreshIntervalMs || this.refreshIntervalMs;
+    this.statusUrl = this.platform.config.statusUrl || '';
+    this.toggleUrl = this.platform.config.toggleUrl || '';
+    this.bearerToken = this.platform.config.bearerToken || '';
+
+    this.platform.log.info('Config: %d %s %s %d', this.refreshIntervalMs, this.statusUrl, this.toggleUrl, this.bearerToken.length);
+
+    if (this.platform.config.bearerToken !== undefined) {
+      this.authHeaders = {
+        headers: {
+          'Authorization': 'Bearer ' + this.platform.config.bearerToken,
+        },
+      };
+    }
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
@@ -32,7 +65,8 @@ export class ExamplePlatformAccessory {
 
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service = this.accessory.getService(this.platform.Service.GarageDoorOpener) ||
+                   this.accessory.addService(this.platform.Service.GarageDoorOpener);
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
@@ -42,13 +76,21 @@ export class ExamplePlatformAccessory {
     // see https://developers.homebridge.io/#/service/Lightbulb
 
     // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    // this.service.getCharacteristic(this.platform.Characteristic.On)
+    //   .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
+    //   .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
 
     // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    // this.service.getCharacteristic(this.platform.Characteristic.Brightness)
+    //   .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.CurrentDoorState)
+      .on('get', this.handleGetCurrentDoorState);
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.TargetDoorState)
+      .on('set', this.handleSetTargetDoorState);
 
     /**
      * Creating multiple services of the same type.
@@ -62,11 +104,11 @@ export class ExamplePlatformAccessory {
      */
 
     // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
+    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
+    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
 
     /**
      * Updating characteristics values asynchronously.
@@ -77,30 +119,159 @@ export class ExamplePlatformAccessory {
      * the `updateCharacteristic` method.
      *
      */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+    // let motionDetected = false;
+    // setInterval(() => {
+    //   // EXAMPLE - inverse the trigger
+    //   motionDetected = !motionDetected;
+    //
+    //   // push the new value to HomeKit
+    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
+    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+    //
+    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
+    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
+    // }, 10000);
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+    var that = this;
+    const fn = async () => {
+      // await this.handleGetCurrentDoorState((_, value) => {
+      //   if (value !== null && value !== undefined) {
+      //     // prevState: at se neprepisuje OPENING a CLOSING (automaticky nastavuje homekit) kdyz se jeste nezmenil stav cidla
+      //     if (this.prevState === null || this.prevState !== value) {
+      //       this.service.updateCharacteristic(this.platform.Characteristic.CurrentDoorState, value);
+      //       this.service.updateCharacteristic(this.platform.Characteristic.TargetDoorState, value);
+      //       this.prevState = value;
+      //     }
+      //   }
+      // });
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+      try {
+        // const { body } = await this.send(this.endpoints.getState);
+        const { body } = await got.get(that.statusUrl);
+        // const state = this.applyMappers(body);
+        let state = that.platform.Characteristic.CurrentDoorState.CLOSED;
+        if (body === 'open') {
+          // await new Promise(f => setTimeout(f, 30000));
+          state = that.platform.Characteristic.CurrentDoorState.OPEN;
+        } else if (body === 'close') {
+          state = that.platform.Characteristic.CurrentDoorState.CLOSED;
+        } else {
+          that.platform.log.error('Got WRONG accessory state %s (%s)', state, body);
+        }
+        that.platform.log.debug('Got accessory state %s (%s)', state, body);
+        // callback(null, state);
+        if (that.prevState === null || that.prevState !== state) {
+          that.service.updateCharacteristic(that.platform.Characteristic.CurrentDoorState, state);
+          that.service.updateCharacteristic(that.platform.Characteristic.TargetDoorState, state);
+          that.prevState = state;
+          this.fixer = 0; // reset
+        }
+        setTimeout(fn, this.refreshIntervalMs);
+      } catch (err) {
+        setTimeout(fn, this.refreshIntervalMs);
+        return that.handleError(err as Error);
+      }
+
+      // this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
+    };
+    setTimeout(fn, this.refreshIntervalMs);
   }
+
+  handleError(err: Error, callback?) {
+    this.platform.log.error(err.message);
+    callback && callback(err);
+  }
+
+  // https://github.com/nicinabox/homebridge-http-entry/blob/master/src/accessory.ts
+  handleGetCurrentDoorState = async (callback: CharacteristicGetCallback) => {
+    // if (!this.endpoints.getState) {
+    //   return callback(null);
+    // }
+
+    // try {
+    //   // const { body } = await this.send(this.endpoints.getState);
+    //   const { body } = await got.get(this.statusUrl);
+    //   // const state = this.applyMappers(body);
+    //   let state = this.platform.Characteristic.CurrentDoorState.CLOSED;
+    //   if (body === 'open') {
+    //     state = this.platform.Characteristic.CurrentDoorState.OPEN;
+    //   } else if (body === 'close') {
+    //     state = this.platform.Characteristic.CurrentDoorState.CLOSED;
+    //   } else {
+    //     this.platform.log.error('Got WRONG accessory state %s (%s)', state, body);
+    //   }
+    //   this.platform.log.debug('Got accessory state %s (%s)', state, body);
+    //   callback(null, state);
+    // } catch (err) {
+    //   return this.handleError(err as Error, callback);
+    // }
+    callback(null, this.prevState);
+  };
+
+  handleSetTargetDoorState = async (
+    value: CharacteristicValue,
+    callback: CharacteristicSetCallback,
+  ) => {
+    try {
+      if (value === this.platform.Characteristic.TargetDoorState.OPEN || value === this.platform.Characteristic.TargetDoorState.CLOSED) {
+        // if (this.prevState === value) { // do not toggle gate e.g. if you want open when is already opened
+        //   this.platform.log.debug('Set target skip already set value %s', value);
+        // } else {
+          await got.post(this.toggleUrl, {
+            ...this.authHeaders,
+          });
+          // this.platform.log.debug('Waiting for sensor confirmation of %s ...', value);
+          //
+          // do {
+          //   await new Promise(f => setTimeout(f, 100));
+          //   this.platform.log.debug('Nope ...  %s !== %s ...', this.prevState, value);
+          // } while (this.prevState !== value);
+
+
+        // if (value === this.platform.Characteristic.TargetDoorState.OPEN) {
+        //   let tmp = this.service.getCharacteristic(this.platform.Characteristic.TargetDoorState).;
+        //   this.platform.log.info("HERE %s", tmp);
+
+          // if (tmp === this.platform.Characteristic.CurrentDoorState.CLOSING) {
+            // this.service.updateCharacteristic(this.platform.Characteristic.CurrentDoorState, this.platform.Characteristic.CurrentDoorState.CLOSING);
+            // this.service.updateCharacteristic(this.platform.Characteristic.TargetDoorState, this.platform.Characteristic.TargetDoorState.CLOSED);
+          // }
+        // }
+
+          this.platform.log.debug('Set accessory state %s', value);
+        // }
+      } else {
+        this.platform.log.error('Set target WRONG value %s', value);
+      }
+
+      callback(null);
+      if (this.prevState === this.platform.Characteristic.CurrentDoorState.OPEN) {
+        if (value === this.platform.Characteristic.TargetDoorState.OPEN) {
+          if (this.fixer % 2 === 0) {
+            this.service.updateCharacteristic(this.platform.Characteristic.TargetDoorState, this.platform.Characteristic.TargetDoorState.CLOSED);
+            this.fixer++;
+          } else {
+            this.service.updateCharacteristic(this.platform.Characteristic.TargetDoorState, this.platform.Characteristic.TargetDoorState.OPEN);
+            this.fixer++;
+          }
+        }
+      }
+
+    } catch (err) {
+      return this.handleError(err as Error, callback);
+    }
+  };
 
   /**
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-  }
+  // async setOn(value: CharacteristicValue) {
+  //   // implement your own code to turn your device on/off
+  //   this.exampleStates.On = value as boolean;
+  //
+  //   this.platform.log.debug('Set Characteristic On ->', value);
+  // }
 
   /**
    * Handle the "GET" requests from HomeKit
@@ -115,27 +286,27 @@ export class ExamplePlatformAccessory {
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
-  }
+  // async getOn(): Promise<CharacteristicValue> {
+  //   // implement your own code to check if the device is on
+  //   const isOn = this.exampleStates.On;
+  //
+  //   this.platform.log.debug('Get Characteristic On ->', isOn);
+  //
+  //   // if you need to return an error to show the device as "Not Responding" in the Home app:
+  //   // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+  //
+  //   return isOn;
+  // }
 
   /**
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, changing the Brightness
    */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-  }
+  // async setBrightness(value: CharacteristicValue) {
+  //   // implement your own code to set the brightness
+  //   this.exampleStates.Brightness = value as number;
+  //
+  //   this.platform.log.debug('Set Characteristic Brightness -> ', value);
+  // }
 
 }
